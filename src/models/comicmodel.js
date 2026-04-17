768573000f8b77e8f272e30288894e122b05a241
dfigarski <d.figarski96@gmail.com>
2026-04-17 18:37:25 +0200

    dodanie danych

---
+++ src/models/comicmodel.js
@@ -0,0 +1,7 @@
+export const comicModelExample = {
+  id: 1,
+  title: 'Amazing Spider-Man',
+  description: 'Opis komiksu Marvel.',
+  thumbnail: 'https://via.placeholder.com/150',
+  year: 2022,
+};
