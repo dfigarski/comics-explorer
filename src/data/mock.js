32977e025baef9cc189791117b64479c8f613923
dfigarski <d.figarski96@gmail.com>
2026-04-17 18:41:39 +0200

    dodawanie kolejnych danych

---
+++ src/data/mock.js
@@ -0,0 +1,23 @@
+export const mockComics = [
+  {
+    id: 1,
+    title: 'Amazing Spider-Man',
+    description: 'Spider-Mana walczącego z cieniem mgły.',
+    thumbnail: 'https://via.placeholder.com/150',
+    year: 2022,
+  },
+  {
+    id: 2,
+    title: 'Iron Man',
+    description: 'Tony Stark"',
+    thumbnail: 'https://via.placeholder.com/150',
+    year: 2021,
+  },
+  {
+    id: 3,
+    title: 'Captain America',
+    description: 'Nowa historia Kapitana Ameryki.',
+    thumbnail: 'https://via.placeholder.com/150',
+    year: 2023,
+  },
+];
