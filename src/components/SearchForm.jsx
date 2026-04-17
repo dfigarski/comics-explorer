8e116132d08544ce1787ee3b190aef340c429e75
dfigarski <d.figarski96@gmail.com>
2026-04-17 19:48:41 +0200

    implementacja formularza z react-hook-form edytowanie pliku searchform

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,9 +1,33 @@
-function SearchForm() {
+import { useForm } from 'react-hook-form';
+
+function SearchForm({ onSearch }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    onSearch(data.title);
+  };
+
   return (
-    <div>
-      <input type="text" placeholder="Wpisz tytuł komiksu" />
-      <button>Szukaj</button>
-    </div>
+    <form className="search-box" onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Wpisz tytuł komiksu..."
+        {...register('title', {
+          required: 'Pole jest wymagane',
+          minLength: {
+            value: 2,
+            message: 'Wpisz co najmniej 2 znaki',
+          },
+        })}
+      />
+      <button type="submit">Szukaj</button>
+
+      {errors.title && <p className="form-error">{errors.title.message}</p>}
+    </form>
   );
 }
 
