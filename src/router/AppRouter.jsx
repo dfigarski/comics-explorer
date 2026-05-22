26c6a5427db9d6a0df62f8eb035eccb0e103a783
Dominik Figarski <122653045+dfigarski@users.noreply.github.com>
2026-05-22 18:20:07 +0200

    feat:dodanie layout

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 import SearchPage from '../pages/SearchPage';
 import DetailsPage from '../pages/DetailsPage';
 import CollectionPage from '../pages/CollectionPage';
@@ -6,12 +7,16 @@ import CollectionPage from '../pages/CollectionPage';
 function AppRouter() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/search" replace />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/item/:id" element={<DetailsPage />} />
-        <Route path="/collection" element={<CollectionPage />} />
-      </Routes>
+      <div className="container">
+        <h1>Marvel Comics Explorer</h1>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/search" replace />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/item/:id" element={<DetailsPage />} />
+          <Route path="/collection" element={<CollectionPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
