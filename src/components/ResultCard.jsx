66a2784240b119d45407124545206f28c24bb484
Dominik Figarski <122653045+dfigarski@users.noreply.github.com>
2026-05-22 18:53:13 +0200

    feat: przejscie z listy wynikow do szczegolow

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom';
+
 function ResultCard({ comic }) {
   return (
-    <div className="card">
-      <img src={comic.thumbnail} alt={comic.title} />
-      <h3>{comic.title}</h3>
-      <p>{comic.description}</p>
-      <p>Rok: {comic.year}</p>
-    </div>
+    <Link to={`/item/${comic.id}`} className="card-link">
+      <div className="card">
+        <img src={comic.thumbnail} alt={comic.title} />
+        <h3>{comic.title}</h3>
+        <p>{comic.description}</p>
+        <p>Rok: {comic.year}</p>
+      </div>
+    </Link>
   );
 }
 
